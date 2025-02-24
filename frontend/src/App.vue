8bffb914c4116bbfbf93adeca3d0f253f0dd8a54
Nicolas-FelSi <nicolasfelipe3702@gmail.com>
2025-02-24 01:31:15 -0300

    .

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,32 +1,30 @@
 <template>
   <div class="d-flex align-items-center justify-content-center h-100">
     <div class="bg-primary-subtle p-4 rounded-1">
-      <form>
+      <form @submit.prevent="adicionarTarefa" method="post">
         <div class="input-group mb-3">
-          <input type="text" class="form-control" placeholder="Adicionar tarefa" aria-label="Adicionar tarefa" aria-describedby="adicionar tarefa">
-          <span class="input-group-text" role="button">
+          <input type="text" class="form-control" name="titulo" placeholder="Adicionar tarefa" v-model="tituloTarefa">
+          <button type="submit" class="input-group-text">
             <i class="bi bi-plus-lg"></i>
-          </span>
+          </button>
         </div>
       </form>
       <section>
-        <div class="d-flex align-items-center justify-content-between bg-black p-2 rounded-1 text-light">
-          <input type="checkbox" name="" id="" role="button">
-          <p class="m-0">Tarefa</p>
+        <div class="d-flex align-items-center justify-content-between bg-black p-2 rounded-1 text-light" v-for="tarefa in tarefas" :key="tarefa.id" v-if="tarefas != ''">
+          <input type="checkbox" name="tarefa" role="button">
+          <p class="m-0">{{ tarefa.titulo }}</p>
           <div>
-            <i class="bi bi-pencil-square text-warning me-3 fs-5"></i>
-            <i class="bi bi-x-circle-fill text-danger fs-5"></i>
+            <i class="bi bi-pencil-square text-warning me-3 fs-5" role="button"></i>
+            <i class="bi bi-x-circle-fill text-danger fs-5" role="button"></i>
           </div>
         </div>
+        <p v-else class="text-center m-0">Nenhuma tarefa cadastrada</p>
       </section>
     </div>
   </div>
 </template>
 
 <script setup>
-const tasks = await fetch("http://127.0.0.1:5000", {
-  method: "GET"
-}).then(response => response.json())
 
 </script>
 
